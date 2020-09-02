919ee5c603da2d2a22061193638ad3a5f8a12a57
PoconoNeo <david@bordne.com>
2020-09-02 08:51:21 -0400

    Added ThrottleBandwidth.ps1

---
+++ Tools/ThrottleBandwidth.ps1
@@ -0,0 +1,7 @@
+﻿powershell.exe -executionpolicy bypass "New-netqospolicy -Name "HTTP" -IPPort 80 -IPProtocol TCP -ThrottleRateActionBitsPerSecond 1000000KB"
+powershell.exe -executionpolicy bypass "New-netqospolicy -Name "HTTPS" -IPPort 443 -IPProtocol TCP -ThrottleRateActionBitsPerSecond 1000000KB"
+
+pause
+
+Remove-NetQosPolicy "HTTPS" -a
+Remove-NetQosPolicy "HTTP" -a
